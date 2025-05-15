ce66137346107974464746bd50b574a3fe0d8f63
gabriel.lazevedo <minifinikogama@gmail.com>
2025-05-15 08:45:17 -0300

    Início do projeto

---
+++ src/main/java/com/projeto/unify/UniFyApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.unify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniFyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniFyApplication.class, args);
+    }
+
+}
